31c4f3927f7fc15c7b6aa3fb5078578a1590f435
niszco <nico.marszalek@gmail.com>
2023-03-21 11:46:16 +0100

    Fixes auth

--- src/view/authentification.view.php
+++ src/view/authentification.view.php
@@ -14,10 +14,16 @@
     </header>
     <section>
         <div id="login-page">
+            <fieldset>
+            <legend>Connexion :</legend>
             <label for="login">Numéro de permis :</label>
             <input type="text" name="login" placeholder="Entrez votre numéro de permis ici" value="<?= $identifiants['login']?>">
+            <br><br>
             <label for="password">Mot de passe :</label>
-            <input type="text" name="password" placholder="Entrez votre mot de passe ici" value="<?= $identifiants['password']?>">
+            <input type="text" name="password" placeholder="Entrez votre mot de passe ici" value="<?= $identifiants['password']?>">
+            <br><br>            
+            <input type="submit" id="submit" value="Connexion">
+            </fieldset>
         </div>
     </section>
 </body>
