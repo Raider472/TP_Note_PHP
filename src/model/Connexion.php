ae0f08e9f93d394d8c38ecd34d6eb148f5b4f72b
Raider <gabrielnae1@gmail.com>
2023-03-26 21:56:39 +0200

    Modification code

--- src/model/Connexion.php
+++ src/model/Connexion.php
@@ -7,13 +7,8 @@
             $db_config["DB_NAME"] = $dbName;
             $db_config["USER"] = $UserPassword;
             $db_config["PASSWORD"] = $UserPassword;
-            try {
-                $this->db = new PDO($db_config["SGBD"] . ":host=" . $db_config["HOST"] . ";dbname=" . $db_config["DB_NAME"], $db_config["USER"], $db_config["PASSWORD"], array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
-                unset($db_config);
-            }
-            catch (PDOException $e) {
-                echo $e->getMessage();
-            }
+            $this->db = new PDO($db_config["SGBD"] . ":host=" . $db_config["HOST"] . ";dbname=" . $db_config["DB_NAME"], $db_config["USER"], $db_config["PASSWORD"], array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+            unset($db_config);
         }
         function execSQL(string $req, array $valeur=[]) : array {
             try {
@@ -36,13 +31,8 @@
             $db_config["DB_NAME"] = $dbName;
             $db_config["USER"] = $User;
             $db_config["PASSWORD"] = $Password;
-            try {
-                $this->db = new PDO($db_config["SGBD"] . ":host=devbdd.iutmetz.univ-lorraine.fr;port=3306" . ";dbname=" . $db_config["DB_NAME"], $db_config["USER"], $db_config["PASSWORD"], array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
-                unset($db_config);
-            }
-            catch (PDOException $e) {
-                echo $e->getMessage();
-            }
+            $this->db = new PDO($db_config["SGBD"] . ":host=devbdd.iutmetz.univ-lorraine.fr;port=3306" . ";dbname=" . $db_config["DB_NAME"], $db_config["USER"], $db_config["PASSWORD"], array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+            unset($db_config);
         }
         function execSQL(string $req, array $valeur=[]) : array {
             try {
--- src/model/FonctionConnexion.php
+++ src/model/FonctionConnexion.php
@@ -3,17 +3,19 @@
 
     function choixConnexion(): ConnexionDocker | ConnexionIUT {
         try {
-            $dbo = new ConnexionIUT("mysql", "nae1u_Infraction", "nae1u_appli", "31904250");
+            $dbo = new ConnexionDocker("mysql", "dbTp", "Infraction");
+            return $dbo;
         }
         catch (PDOException $e) {
             echo $e->getMessage();
         }
         try {
-            $dbo = new ConnexionDocker("mysql", "dbTp", "Infraction");
+            $dbo = new ConnexionIUT("mysql", "nae1u_Infraction", "nae1u_appli", "31904250");
+            return $dbo;
         }
         catch (PDOException $e) {
             echo $e->getMessage();
         }
-        return $dbo;
+        throw new Exception("Aucune connexion possible");
     }
 ?>
