dd68c2f3623c625e75c89e31b5b87d995d3063dd
Raider472 <gabrielnae1@gmail.com>
2023-03-28 10:00:19 +0200

    Les conducteurs Exemple

--- src/model/LesConducteurs.php
+++ src/model/LesConducteurs.php
@@ -10,9 +10,18 @@
 
         function fetchAllConducteur() {
             $dbo = choixConnexion();
-            $requete = $dbo->execSQL("SELECT * FROM conducteur");
+            $requete = $dbo->execSQL("SELECT * FROM conducteur"); //Recupère tous les conducteur de la table Conducteur
             $conducteur = [];
-            var_dump($requete);
+            foreach($requete as $lesrequeteTab) { //Créer les conducteurs grâce à la classe conducteur et les push dans l'array $conducteur
+                $conducteur[] = new Conducteur(
+                    $lesrequeteTab["no_permis"],
+                    $lesrequeteTab["date_permis"],
+                    $lesrequeteTab["nom"],
+                    $lesrequeteTab["prenom"],
+                    $lesrequeteTab["mdp"]
+                );
+            }
+            echo $conducteur[1]->getMdp(); //Recupère et Affiche le mdp du deuxième conducteur
         }
     }
 ?>
