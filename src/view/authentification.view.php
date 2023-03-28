73cc2f84d5b66dc4a533a9ce3dc4e8653b4e1f14
niszco <nico.marszalek@gmail.com>
2023-03-28 10:00:43 +0200

    Fix css authentification.view

--- src/view/authentification.view.php
+++ src/view/authentification.view.php
@@ -17,9 +17,11 @@
             <fieldset>
             <legend>Connexion :</legend>
             <label for="login">Numéro de permis :</label>
+            <br>
             <input type="text" name="login" placeholder="Entrez votre numéro de permis ici" value="<?= $identifiants['login']?>">
             <br><br>
             <label for="password">Mot de passe :</label>
+            <br>
             <input type="text" name="password" placeholder="Entrez votre mot de passe ici" value="<?= $identifiants['password']?>">
             <br><br>            
             <input type="submit" id="submit" value="Connexion">
