9324335a00cdfa1a2c8a9ab123f6cff6aed9cac3
Raider <gabrielnae1@gmail.com>
2023-04-10 16:36:06 +0200

    Inversion connexion

--- src/model/function/FonctionConnexion.php
+++ src/model/function/FonctionConnexion.php
@@ -3,14 +3,14 @@
 
     function choixConnexion(): ConnexionDocker | ConnexionIUT {
         try {
-            $dbo = new ConnexionDocker("mysql", "dbTp", "Infraction");
+            $dbo = new ConnexionIUT("mysql", "nae1u_Infraction", "nae1u_appli", "31904250");
             return $dbo;
         }
         catch (PDOException $e) {
             echo $e->getMessage();
         }
         try {
-            $dbo = new ConnexionIUT("mysql", "nae1u_Infraction", "nae1u_appli", "31904250");
+            $dbo = new ConnexionDocker("mysql", "dbTp", "Infraction");
             return $dbo;
         }
         catch (PDOException $e) {
